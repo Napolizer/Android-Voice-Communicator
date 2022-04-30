a70dae3828664b633390a6620509e91dacd1c6d2
napolizer <napolizer@op.pl>
2022-04-30 18:16:44 +0200

    Podstawowa funkcjonalność, możliwość wykonywania połączeń głosowych pomiędzy dwoma urządzeniami.

---
+++ app/src/main/java/com/example/tk_zadanie4/Call.java
@@ -0,0 +1,197 @@
+package com.example.tk_zadanie4;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
+import android.media.MediaRecorder;
+
+import androidx.core.app.ActivityCompat;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Call {
+    private MainActivity parentActivity;
+
+    private InetAddress externalIpAddress;
+    private int externalPortNumber;
+
+    private DatagramSocket server;
+
+    private int sampleRate;
+    private int sampleInterval;
+    private int sampleSize;
+
+    private boolean microphoneRunning;
+    private boolean speakersRunning;
+
+    private Thread microphoneThread;
+    private Thread speakersThread;
+
+    private Call(MainActivity parentActivity, String externalIpAddress, int externalPortNumber, DatagramSocket server,
+                 int sampleRate, int sampleInterval, int sampleSize) throws UnknownHostException {
+        this.parentActivity = parentActivity;
+        this.externalIpAddress = InetAddress.getByName(externalIpAddress);
+        this.externalPortNumber = externalPortNumber;
+        this.server = server;
+        this.sampleRate = sampleRate;
+        this.sampleInterval = sampleInterval;
+        this.sampleSize = sampleSize;
+    }
+
+    public static Builder getBuilder() {
+        return new Builder();
+    }
+
+    static class Builder {
+        public MainActivity parentActivity;
+
+        public String externalIpAddress;
+        public int externalPortNumber;
+
+        public DatagramSocket server;
+
+        public int sampleRate;
+        public int sampleInterval;
+        public int sampleSize;
+
+        private Builder() {
+
+        }
+
+        public Call build() throws UnknownHostException {
+            return new Call(parentActivity, externalIpAddress, externalPortNumber, server, sampleRate, sampleInterval, sampleSize);
+        }
+    }
+
+    public void startCall() {
+        endCall();
+
+        turnOnMicrophone();
+        turnOnSpeakers();
+    }
+
+    public void endCall() {
+        try {
+            stopMicrophone();
+            stopSpeakers();
+        } catch(InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void turnOnMicrophone() {
+        startRecording();
+    }
+
+    public void turnOnSpeakers() {
+        startReceiving();
+    }
+
+    public void turnOffMicrophone() {
+        microphoneRunning = false;
+    }
+
+    public void turnOffSpeakers() {
+        speakersRunning = false;
+    }
+
+    private void stopMicrophone() throws InterruptedException {
+        if (microphoneRunning) {
+            microphoneRunning = false;
+            microphoneThread.join();
+        }
+    }
+
+    private void stopSpeakers() throws InterruptedException {
+        if (speakersRunning) {
+            speakersRunning = false;
+            speakersThread.join();
+        }
+    }
+
+    private void startRecording() {
+        if (!microphoneRunning) {
+            if (ActivityCompat.checkSelfPermission(parentActivity.getContext(), Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+                microphoneThread = new Thread(() -> {
+                    microphoneRunning = true;
+
+                    AudioRecord microphone = new AudioRecord(
+                            MediaRecorder.AudioSource.VOICE_COMMUNICATION,
+                            sampleRate, AudioFormat.CHANNEL_IN_MONO,
+                            AudioFormat.ENCODING_PCM_16BIT, AudioRecord.getMinBufferSize(
+                            sampleRate, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT)*10);
+
+                    final int bufferSize = sampleInterval * sampleInterval * sampleSize * 2;
+                    byte[] buffer = new byte[bufferSize];
+
+                    try {
+                        DatagramSocket socket = new DatagramSocket();
+
+                        microphone.startRecording();
+
+                        while (microphoneRunning) {
+                            int bytesRead = microphone.read(buffer, 0, bufferSize);
+                            DatagramPacket packet = new DatagramPacket(buffer, bytesRead, externalIpAddress, externalPortNumber);
+                            socket.send(packet);
+                            Thread.sleep(sampleInterval, 0);
+                        }
+
+                        microphone.stop();
+                        microphone.release();
+                        socket.disconnect();
+                        socket.close();
+                    } catch(Exception e) {
+                        microphoneRunning = false;
+                        throw new RuntimeException(e);
+                    }
+                });
+                microphoneThread.start();
+            } else {
+                ActivityCompat.requestPermissions(parentActivity, new String[]{Manifest.permission.RECORD_AUDIO}, 0);
+                startRecording();
+            }
+        } else {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    private void startReceiving() {
+        if (!speakersRunning) {
+            speakersRunning = true;
+            speakersThread = new Thread(() -> {
+                final int bufferSize = sampleInterval * sampleInterval * sampleSize * 2;
+                byte[] buffer = new byte[bufferSize];
+
+                AudioTrack audio = new AudioTrack(AudioManager.STREAM_MUSIC, sampleRate,
+                        AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
+                audio.play();
+                try {
+                    while (speakersRunning) {
+                        DatagramPacket packet = new DatagramPacket(buffer, bufferSize);
+                        server.receive(packet);
+                        audio.write(packet.getData(), 0, bufferSize);
+                    }
+
+                    server.disconnect();
+                    server.close();
+                    audio.stop();
+                    audio.flush();
+                    audio.release();
+                } catch(Exception e) {
+                    speakersRunning = false;
+                    throw new RuntimeException(e);
+                }
+            });
+
+            speakersThread.start();
+        } else {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tk_zadanie4/MainActivity.java
@@ -0,0 +1,167 @@
+package com.example.tk_zadanie4;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.text.format.Formatter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final Pattern IP_ADDRESS
+            = Pattern.compile(
+            "((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\\.(25[0-5]|2[0-4]"
+                    + "[0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]"
+                    + "[0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}"
+                    + "|[1-9][0-9]|[0-9]))");
+
+    private ConstraintLayout layout;
+    private TableLayout tableLayout;
+    private EditText textYourIPAddress;
+    private TextView textYourPortNumber;
+    private Button startCall;
+    private EditText externalIpAddress;
+    private EditText externalPortNumber;
+
+    private Context context;
+    private WifiManager wifiManager;
+
+    private DatagramSocket server;
+
+    protected void initComponents() {
+        layout = findViewById(R.id.Layout);
+        tableLayout = findViewById(R.id.TableLayout);
+        textYourIPAddress = findViewById(R.id.TextYourIPAddress);
+        textYourPortNumber = findViewById(R.id.TextYourPortNumber);
+        startCall = findViewById(R.id.ButtonStartCall);
+        externalIpAddress = findViewById(R.id.EditTextExternalIpAddress);
+        externalPortNumber = findViewById(R.id.EditTextExternalPortNumber);
+
+        context = getApplicationContext();
+        wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+
+        try {
+            createServer();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected MainActivity getMainActivity() {
+        return this;
+    }
+
+    protected String getIpAddress() {
+        return Formatter.formatIpAddress(wifiManager.getConnectionInfo().getIpAddress());
+    }
+
+    protected String getPortNumber() {
+        return String.valueOf(server.getLocalPort());
+    }
+
+    protected boolean isDeviceConnected() {
+        return wifiManager.isWifiEnabled();
+    }
+
+    protected void createServer() throws SocketException, UnknownHostException {
+        server = new DatagramSocket(0, InetAddress.getByName(getIpAddress()));
+    }
+
+    protected boolean validateUserInput() {
+        try {
+            Matcher matcher = IP_ADDRESS.matcher(externalIpAddress.getText().toString());
+            if (!matcher.matches()) {
+                throw new RuntimeException();
+            }
+            Integer.parseInt(externalPortNumber.getText().toString());
+            return true;
+        } catch(RuntimeException e) {
+            AlertDialog.Builder invalidInputDialog = new AlertDialog.Builder(this);
+            invalidInputDialog.setMessage("Port Number or Ip Address is incorrect");
+            invalidInputDialog.setTitle("Invalid Input");
+            invalidInputDialog.setCancelable(false);
+
+            invalidInputDialog.setPositiveButton("OK", (dialogInterface, i) -> {});
+            invalidInputDialog.create().show();
+        }
+        return false;
+    }
+
+    protected boolean doesServerExists(String ip, int port) {
+        try {
+            return InetAddress.getByName(ip).isReachable(2000);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initComponents();
+
+        if (isDeviceConnected()) {
+            textYourIPAddress.setText(getIpAddress());
+        } else {
+            AlertDialog.Builder deviceNotConnectedDialog = new AlertDialog.Builder(this);
+            deviceNotConnectedDialog.setMessage("Device is not connected to internet");
+            deviceNotConnectedDialog.setTitle("No Connection");
+            deviceNotConnectedDialog.setCancelable(false);
+
+            deviceNotConnectedDialog.setPositiveButton("OK", (dialogInterface, i) -> {
+                moveTaskToBack(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(0);
+            });
+
+
+            deviceNotConnectedDialog.create().show();
+        }
+
+        textYourPortNumber.setText(getPortNumber());
+
+        startCall.setOnClickListener(view -> {
+            if (validateUserInput()) {
+                Call.Builder builder = Call.getBuilder();
+
+                builder.parentActivity = getMainActivity();
+                builder.externalIpAddress = externalIpAddress.getText().toString();
+                builder.externalPortNumber = Integer.parseInt(externalPortNumber.getText().toString());
+                builder.server = server;
+                builder.sampleRate = 8000;
+                builder.sampleInterval = 20;
+                builder.sampleSize = 2;
+
+                Call call;
+                try {
+                    call = builder.build();
+                } catch (UnknownHostException e) {
+                    throw new RuntimeException(e);
+                }
+
+                call.startCall();
+            }
+        });
+    }
+
+    public Context getContext() {
+        return context;
+    }
+}
